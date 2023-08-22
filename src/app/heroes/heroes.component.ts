37c3daffa0815f7fc10943d847496c63c720b6e4
Alejandra Sanabria <ma.sanabrias@uniandes.edu.co>
2023-08-21 22:51:41 -0500

    Add:List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,3 @@
-import { HEROES } from '../heroes';
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -9,10 +8,17 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero:Hero={
-    id:1,
-    name:'Windstorm'
-  };
-   heroes:HEROES;
-}
 
+  HEROES:Hero[]=[
+    {id:1, name:'Ghost'},
+    {id:2, name:'Alias-Battleworn'},
+    {id:3, name:'Dusk'},
+    {id:4, name:'Manta Ray'},
+    {id:5, name:'Viktor Reznov'},
+    {id:6, name:'Cassius'},
+    {id:7, name:'Naomi Mizushima'},
+    {id:8, name:'Price-Bravo 6'},
+    {id:9, name:'Beck-Spray Paint'},
+    {id:10, name:'Tempest'}
+]; 
+}
